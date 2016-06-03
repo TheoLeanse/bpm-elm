32450af75a38625cca6d64e68021198a29259a10
TheoLeanse <theoleanse@gmail.com>
2016-06-03 17:06:00 +0100

    model is a record

--- main.elm
+++ main.elm
@@ -3,22 +3,24 @@ import Html.App exposing (beginnerProgram)
 import Html.Events exposing (onClick)
 import Time exposing (now)
 import Date exposing (second)
+import Task exposing (perform)
 
 
 main =
     beginnerProgram { model = model, view = view, update = update }
 
 model =
-    []
+    { beats = []
+    , timestamp = 0
+    }
 
 view model =
     div []
-        [ button [ onClick Increment ] [ text "Click here" ]
+        [ button [ onClick now ] [ text "Click here" ]
         , div [] [ text (toString model) ]
         ]
 
-
 type Msg = Increment
 
-update msg model =
-    model ++ [now]
+update now model =
+    { model | beats = model.beats ++ [now] }
